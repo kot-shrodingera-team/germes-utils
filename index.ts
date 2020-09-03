4c162461b29d40fba4339a43e0af72e31937cc78
kot-shrodingera <kot.shrodingera@gmail.com>
2020-09-03 22:30:57 +0600

    Добавлена функция timeString

--- index.ts
+++ index.ts
@@ -327,3 +327,11 @@ export const nativeInput = (
     keyFunction(charCode);
   });
 };
+
+export const timeString = (time: Date): string => {
+  const hours = String(time.getHours()).padStart(2, '0');
+  const minutes = String(time.getMinutes()).padStart(2, '0');
+  const seconds = String(time.getSeconds()).padStart(2, '0');
+  const miliseconds = String(time.getMilliseconds()).padStart(3, '0');
+  return `${hours}:${minutes}:${seconds}.${miliseconds}`;
+};
