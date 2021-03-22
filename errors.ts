6f2a772d712f7058e1a5a05f13522bf3e1ef9800
kot-shrodingera <kot.shrodingera@gmail.com>
2021-03-22 20:56:47 +0600

    feat(errors): объявление классов ошибок

---
+++ errors.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line max-classes-per-file
+export class JsFailError extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, JsFailError.prototype);
+  }
+}
+
+export class NewUrlError extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, NewUrlError.prototype);
+  }
+}
