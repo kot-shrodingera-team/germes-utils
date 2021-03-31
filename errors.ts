85a61bc1ac2b70959c37057f48f053bfcecd6c07
kot-shrodingera <kot.shrodingera@gmail.com>
2021-04-01 02:00:40 +0600

    feat(error): message теперь необязательный аргумент

--- errors.ts
+++ errors.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line max-classes-per-file
 export class JsFailError extends Error {
-  constructor(message: string) {
+  constructor(message?: string) {
     super(message);
     Object.setPrototypeOf(this, JsFailError.prototype);
   }
 }
 
 export class NewUrlError extends Error {
-  constructor(message: string) {
+  constructor(message?: string) {
     super(message);
     Object.setPrototypeOf(this, NewUrlError.prototype);
   }
