7d2e4baa2033de690b9f19c496b73cdedc97b90b
kot-shrodingera <kot.shrodingera@gmail.com>
2021-04-09 04:43:30 +0600

    refactor(GermesData): отдельный интерфейс

--- windowInterface.ts
+++ windowInterface.ts
@@ -1,6 +1,15 @@
+interface GermesData {
+  bookmakerName: string;
+  doStakeTime: Date;
+  betProcessingStep: string;
+  betProcessingAdditionalInfo: string;
+  betProcessingTimeout: number;
+}
+
 declare global {
   interface Window {
     consoleCopy: Console;
+    germesData: GermesData;
   }
 }
 
